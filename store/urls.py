471026512653dc8c05feb9cbbb974798775ebeed
DESKTOP-DB0LE29\danya <danya.tvorun.88@mail.ru>
2021-05-06 13:12:06 +0300

    Order, cart functionality

--- store/urls.py
+++ store/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 
-from .views import (
-   store_view,
-   cart_view,
-   checkout_view,
-)
+from .views import *
 
 urlpatterns = [
    path('', store_view, name='store'),
    path('cart/', cart_view, name='cart'),
    path('checkout/', checkout_view, name='checkout'),
+   path('update_item/', update_item_view, name='update_item'),
+   path('process_order/', processOrder, name='process_order')
 ]
